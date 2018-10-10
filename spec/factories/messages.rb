f9576963b542590a4d220796b96ec446abbc9c51
sentral1110728 <okamurayuu@okamurayuunoMacBook-Pro.local>
2018-10-10 14:41:22 +0900

    FactoryGirl-data定義

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :message do
+    content Faker::Lorem.sentence
+    image File.open("#{Rails.root}/public/images/no-image.jpg")
+    user
+    group
+  end
+
+end
