42683d6bc9680cc0310c7339f7ad78cd3e1c829e
sentral1110728 <okamurayuu@okamurayuunoMacBook-Pro.local>
2018-10-16 10:06:50 +0900

    Add.doneMethod

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,19 @@
 $(function() {
+  function buildHTML(message) {
+    var html = `<p class="member-name">
+                  ${ message.user_name }
+                </p>
+                <span class="time">
+                  ${ message.created_at }
+                </span>
+                <div class="lower-message">
+                  <p class="member-text">
+                    ${ message.content }
+                  </p>
+                  ${ message.image }
+                </div>`
+    return html
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +25,12 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.chat-space').append(html)
+      $('.member-text').val('')
+      $('.lower-message__image').val('')
     })
   })
 });
