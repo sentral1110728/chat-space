51a51943716550575feeea5959ef8ee785b380df
sentral1110728 <okamurayuu@okamurayuunoMacBook-Pro.local>
2018-10-23 15:39:37 +0900

    Add.setInterval_5秒

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,4 +36,16 @@ $(function() {
       alert('error');
     })
   })
+
+  setInterval(function() {
+  $.ajax({
+    url: location.href.json
+  })
+  .done(function(data) {
+  })
+  .fail(function(data) {
+  });
+  } else {
+clearInterval(interval);
+  } , 5000);
 });
